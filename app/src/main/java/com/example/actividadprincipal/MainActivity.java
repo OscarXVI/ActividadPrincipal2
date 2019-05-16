d98cc9143f06a533a0629927bb3ce6a9963b2f88
OscarXVI <calavera-123>
2019-05-16 12:04:53 -0500

    Se Hizo la modificacion del menu para que sea un menu tranformable

--- app/src/main/java/com/example/actividadprincipal/MainActivity.java
+++ app/src/main/java/com/example/actividadprincipal/MainActivity.java
@@ -5,38 +5,47 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Interpolator;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
-    boolean click = false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                click = !click;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
-                            android.R.interpolator.fast_out_slow_in);
 
-                    view.animate()
-                            .rotation(click ? 45f : 0)
-                            .setInterpolator(interpolador)
-                            .start();
-                }
+
+        private FABToolbarLayout morph;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            morph = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+
+            View uno, dos, tres, cuatro;
+
+            uno = findViewById(R.id.uno);
+            dos = findViewById(R.id.dos);
+            cuatro = findViewById(R.id.cuatro);
+            tres = findViewById(R.id.tres);
+
+            fab.setOnClickListener(this);
+            uno.setOnClickListener(this);
+            dos.setOnClickListener(this);
+            tres.setOnClickListener(this);
+            cuatro.setOnClickListener(this);
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.fab) {
+                morph.show();
             }
-        });
-    }
 
-}
+            morph.hide();
+        }
+    }
