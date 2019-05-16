ba2ccc91ab00d557b5f089dfeaa21845e8ae6828
OscarXVI <calavera-123>
2019-05-16 12:55:25 -0500

    Se Hizo la modificacion del menu para que se convierta en un menu de accion este es el boton final

--- app/src/main/java/com/example/actividadprincipal/MainActivity.java
+++ app/src/main/java/com/example/actividadprincipal/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
 
 
@@ -22,30 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
             setSupportActionBar(toolbar);
 
-            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-            morph = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
 
-            View uno, dos, tres, cuatro;
-
-            uno = findViewById(R.id.uno);
-            dos = findViewById(R.id.dos);
-            cuatro = findViewById(R.id.cuatro);
-            tres = findViewById(R.id.tres);
-
-            fab.setOnClickListener(this);
-            uno.setOnClickListener(this);
-            dos.setOnClickListener(this);
-            tres.setOnClickListener(this);
-            cuatro.setOnClickListener(this);
-        }
-
-
-        @Override
-        public void onClick(View v) {
-            if (v.getId() == R.id.fab) {
-                morph.show();
-            }
-
-            morph.hide();
         }
     }
